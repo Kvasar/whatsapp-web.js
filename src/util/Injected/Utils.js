0146931acdbe2d9df296636bad20ca749228810b
vladyslav_bublyk <kvasar739@gmail.com>
2026-07-16 10:00:47 +0300

    Revert "fix: restore message id._serialized after serialize"
    This reverts commit b7a1cf0e9d386ed7d7f33e16e3aef02f0b4c770f.

--- src/util/Injected/Utils.js
+++ src/util/Injected/Utils.js
@@ -828,21 +828,9 @@ exports.LoadUtils = () => {
             msg.replyButtons = JSON.parse(JSON.stringify(msg.replyButtons));
         }
 
-        if (msg.id) {
-            let remote = msg.id.remote;
-            if (typeof remote === 'object' && remote) {
-                remote = remote._serialized;
-            }
-            // serialize() often drops MessageKey._serialized (prototype getter).
-            // Rebuild it so Node-side message.id._serialized is always present.
-            const serialized =
-                msg.id._serialized ||
-                (remote != null && msg.id.id != null
-                    ? `${msg.id.fromMe}_${remote}_${msg.id.id}`
-                    : undefined);
+        if (typeof msg.id.remote === 'object') {
             msg.id = Object.assign({}, msg.id, {
-                remote,
-                ...(serialized ? { _serialized: serialized } : {}),
+                remote: msg.id.remote._serialized,
             });
         }
 
